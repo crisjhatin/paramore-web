081f3555f3e6ef64291682b9089e83baee7774ff
CristianJhair <cristianjhair2000@gmail.com>
2023-07-03 21:50:41 -0500

    logos-index changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,5 @@
 //definir api
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCc7_woMAIVIW2mAr1rPCsFQ&part=snippet%2Cid&order=date&maxResults=10'
-import API_KEY from ".env"
 
 //agregando referencia del elemento dónde queremos colocar el llamado de la api
 const content=null||document.getElementById("content")
@@ -9,7 +8,7 @@ const content=null||document.getElementById("content")
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Key': '37d94d5ccbmshf3c8f135a3f1889p11ce55jsnebef264b4ccf',
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
 };
@@ -27,18 +26,21 @@ async function fetchData(urlApi){
         const videos = await fetchData(API);
         //generar un template string para la iteración de elementos recuperados en "const=videos"
         let view=`
-        ${videos.items.map(video=>`<a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank"><div class="group relative">
-        <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
-          </h3>
-        </div>
-      </div></a>`).slice(0,4).join('')}
+        ${videos.items.map(video=>`
+        <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+          <div class="group relative">
+            <div
+              class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+              <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+              <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+              </h3>
+            </div>
+          </div>
+        </a>`).slice(0,4).join('')}
         `;
         //añadir arreglo de items a elemento "content"
         content.innerHTML=view;
